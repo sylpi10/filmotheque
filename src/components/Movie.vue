cc35147291a9a47d3738cae55aa741a5fa03d379
sylpi <syl.pillet@hotmail.fr>
2021-01-21 10:24:23 +0100

    create project, consume api, filter movies

---
+++ src/components/Movie.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="card">
+            <slot name="name">
+            </slot>
+            <slot name="year">
+            </slot>
+            <div class="img">
+                <slot name="image"> </slot>
+            </div>
+        <div>
+            <slot name="buttons"> </slot>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "Movie",
+    props: {
+       
+    }
+  
+}
+</script>
+
+<style lang="scss">
+    .card{
+        width: 340px;
+        margin: 20px;
+        border: solid 1px #ccc;
+        border-radius: 10px;
+        h1{
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            background-color: orange;
+            margin-top: 0;
+            padding: 10px;
+        }
+        .img img{
+            max-height: 320px;
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+      <h1>Filmotheque</h1>
+      <span>{{loading}}</span>
+      <div class="search">
+        <label for="movie-title">Find a Movie</label>
+        <input type="search" name="movie-title" v-model="title" id="">
+      </div>
+      <hr>
+      <main class="movies-wrapper">
+      <Movie v-for="movie in filteredList" :key="movie.id">
+           <template v-slot:name>
+            <h1>
+              {{movie.name}}
+            </h1>
+          </template>
+           <template v-slot:year>
+            <h2>
+              {{movie.year}}
+            </h2>
+          </template>
+            <template v-slot:image>
+            <img :src="`${movie.url}`" alt="Movie Poster" width="300">  
+          </template>
+      </Movie> 
+      </main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
+import Movie from "../components/Movie.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    Movie
+  },
+  data(){
+    return {
+      movies: [],
+      loading: '',
+      title: ''
+    };
+  },
+  computed: {
+    filteredList(){
+        return this.movies.filter((movie) => {
+            return movie.name.toLowerCase().includes(this.title.toLowerCase());
+        })
+  },
+  },
+  methods: {
+    async consumeMovieApi() {
+   // GET request using axios with async/await
+      const response = await axios.get("https://movies-api.alexgalinier.now.sh/");
+      this.loading = 'loading movies';
+      this.movies = response.data;
+      this.loading = '';
+    }
+  },
+   beforeMount(){
+    this.consumeMovieApi()
+ },
 };
 </script>
+
+<style lang="scss">
+    .movies-wrapper{
+      width: 74%;
+      margin: auto;
+      display: flex;
+      justify-content: space-around;
+      flex-wrap: wrap;
+    }
+</style>
