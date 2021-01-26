51662e46455bcc4c0f7d92b0f881347c12255ca6
sylpi <syl.pillet@hotmail.fr>
2021-01-26 12:21:52 +0100

    add filters by name and date

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -3,8 +3,24 @@
         <div class="">
             <span v-if="loading" class="loader">Loading movies...</span>
             <div class="search">
+              <div class="">
                 <label for="movie-title">Find a Movie:</label>
                 <input type="search" name="movie-title" v-model="title" placeholder="Ex:Usual Suspect">
+              </div>
+                <!-- <select name="Filter" id="" :change="applyFilter()" v-model="selectedFilter"> 
+                  <option >Filter</option>
+                  <option value="byName" >By Name</option>
+                  <option value="byDate">By Date</option>
+                </select> -->
+                <button class="filterBtn" @click="filterByName()">By Name</button>
+                <button class="filterBtn" @click="filterByDate()">By Date</button>
+                <div class="select">
+                <select name="direction" id="" v-model="direction">
+                  <option selected="selected" value="asc">Asc </option>
+                  <option value="dsc">Dsc</option>
+                </select>
+               
+                </div>
             </div>
         </div>
         <div class="movies-wrapper">
@@ -46,6 +62,7 @@ export default {
       movies: [],
       loading: false,
       title: '',
+      direction: 'asc'
     };
   },
   filters: {
@@ -69,9 +86,24 @@ export default {
       this.loading = true;
       const response = await axios.get("https://movies-api.alexgalinier.now.sh/");
       // sort movies by name
-      this.movies = await response.data.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+      this.movies = await response.data;
       this.loading = await false;
     },
+    filterByName(){
+      if (this.direction == "asc") {
+        this.movies.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+      }else if (this.direction == "dsc") {
+        this.movies.sort((b, a) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+      }
+    },
+    filterByDate(){
+       if (this.direction == "asc") {
+        this.movies.sort((a, b) => a.year.localeCompare(b.year));
+       } else  if (this.direction == "dsc") {
+         this.movies.sort((b, a) => a.year.localeCompare(b.year));
+       }
+    },
+
     toUpdate(idToUpdate){
        this.$router.push(`edit-movie/${idToUpdate}`) 
     //   this.$router.push(`edit/${idToUpdate}`) 
@@ -104,8 +136,29 @@ export default {
     }
     .search{
       margin: 20px;
+      display: flex;
+      width: 40%;
+      margin: auto;
+      justify-content: space-around;
+      align-items: center;
       label{
         margin-right: 10px;
       }
-    }
+      button{
+        border: none;
+        background-color: rgb(16, 93, 238);
+        color: #fff;
+        border-radius: 4px;
+        cursor: pointer;
+        padding:7px 10px;
+      }
+      select{
+        border: none;
+        background-color: lightgreen;
+        padding:4px 20px;
+        appearance: none; 
+        height: 30px;
+      }
+
+  }
 </style>
